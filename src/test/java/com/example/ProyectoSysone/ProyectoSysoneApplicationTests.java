d5d1ff68c9f69e89a84e319b8c0a6347ec28b921
meridacindy98 <meridacindy98@yahoo.com.ar>
2020-12-05 18:05:26 -0300

    Corren los tests

--- src/test/java/com/example/ProyectoSysone/ProyectoSysoneApplicationTests.java
+++ src/test/java/com/example/ProyectoSysone/ProyectoSysoneApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoSysoneApplicationTests {
+public class ProyectoSysoneApplicationTests {
 
 	@Test
 	void contextLoads() {
